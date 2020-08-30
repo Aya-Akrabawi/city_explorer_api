a6b5c9d1926368d6e0e713e76246c96f6c1dec15
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-08-30 21:11:48 +0300

    complete all the requirments

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict'
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000
-app.listen(PORT , ()=>{
-    console.log(`You are listening to port ${PORT}`)
-})
+app.use(cors());
+
 
 app.get('/location',( request , respond) =>{
-    const geoData = require('./data/geo.json');
+    const geoData = require('./data/location.json');
     console.log(geoData);
     const cityData = request.query.city;
     console.log(cityData);
@@ -18,11 +18,42 @@ app.get('/location',( request , respond) =>{
     //     "longitude": "-122.332071"
     //   }
     let locationData = new Location(cityData,geoData);
-    res.send(locationData);
+    respond.send(locationData);
 })
-function Location(cityData) {
+function Location(cityData,geoData) {
     this.search_query = cityData;
     this.formatted_query=geoData[0].display_name;
     this.latitude=geoData[0].lat;
     this.longitude=geoData[0].lon;
 }
+
+app.get('/weather',( req , res) =>{
+    // let weatherArr = []
+    const cityData = req.query.city;
+    const weatherInfo = require('./data/weather.json');
+    console.log(weatherInfo);
+    let weatherArr = [];
+    weatherInfo.data.forEach(element => {
+    let forecast = element.weather.description;
+    let time = element.datetime;
+    weatherArr.push(new Weather (forecast,time))
+})
+res.send(weatherArr);
+});
+
+function Weather(forecast,time) {
+    this.forecast = forecast;
+    this.time=time;
+    // Weather.all.push(this);
+    // weatherArr.push(this)
+}
+app.use('*',(req,res)=>{
+    res.status(404).send('NOT FOUND');
+})
+
+app.use((error,req,res)=>{
+    res.status(500).send(`Oops! There is a ${error} error`);
+})
+app.listen(PORT , ()=>{
+    console.log(`You are listening to port ${PORT}`)
+})
